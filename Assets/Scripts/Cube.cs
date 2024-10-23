9478a67c9b9da4a9894b6d267a9799837d7cb050
alejandrocy99 <moflycy@gmail.com>
2024-10-23 19:13:08 +0200

    añadir mapa procedural

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Physics.Raycast(transform.position, transform.up))
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+}
--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -1,62 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO.Compression;
+using UnityEditor;
 using UnityEngine;
 
 public class Generate : MonoBehaviour
 {
-    public int x;
-    public int y;
-    public int z;
+
     public int baseSize;
 
     public int steps;
-    public GameObject cubo;
     private GameObject[,] map; // Declarar el array de GameObjects
+
+    public GameObject cube;
+    public int with, height, large;
+    public float detail;
+    public int seed;
 
     // Start is called before the first frame update
     void Start()
     {
         // Inicializar el array con el tamaño adecuado
-        map = new GameObject[x, y];
-        //GenerateMap();
-       //GeneratePyramid();
-       GenerateLadder();
+        seed = Random.Range(100000, 900000);
+        GenerateMap();
+        //GeneratePyramid();
+        //GenerateLadder();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void GenerateMap()
     {
-        for (int i = 0; i < x; i++)
+        // Iterar sobre las coordenadas x, z (horizontalmente en el plano)
+        for (int x = 0; x < with; x++)
         {
-            for (int j = 0; j < y; j++)
+            for (int z = 0; z < large; z++)
             {
-                map[i, j] = Instantiate(cubo, new Vector3(i, 0, j), Quaternion.identity);
-                Generate cuboComponent = map[i, j].GetComponent<Generate>();
-                if (cuboComponent != null)
+                // Calcular la altura usando PerlinNoise, pero sin modificar height
+                height = (int)(Mathf.PerlinNoise((x / 2 + seed) / detail, (z / 2 + seed) / detail) * detail);
+                for (int y = 0; y < height; y++)
                 {
-                    cuboComponent.x = i;
-                    cuboComponent.y = j;
+                    // Instanciar el cubo en la posición (x, y, z), con y representando la altura
+                    Instantiate(cube, new Vector3(x, y, z), Quaternion.identity);
                 }
+
             }
         }
     }
 
 
 
-   public void GeneratePyramid()
+    public void GeneratePyramid()
     {
         // Para cada nivel de la pirámide
         for (int y = 0; y < baseSize; y++)
         {
             // El tamaño del nivel en X y Z disminuye conforme subimos  levelsize determina el numero de cubos que hay en ese nivel 
             int levelSize = baseSize - y;
-            
+
             // Calcular el offset para centrar el nivel en la base
             float offset = (baseSize - levelSize) / 2.0f;
 
@@ -67,7 +72,7 @@ public class Generate : MonoBehaviour
                 {
                     // Instanciar el cubo en la posición (x, y, z) con el offset calculado para centrar
                     Vector3 position = new Vector3(x + offset, y, z + offset);
-                    Instantiate(cubo, position, Quaternion.identity);
+                    Instantiate(cube, position, Quaternion.identity);
                 }
             }
         }
@@ -75,23 +80,28 @@ public class Generate : MonoBehaviour
 
 
 
-       public void GenerateLadder()
+    public void GenerateLadder()
     {
-         // Para cada nivel de escalón
+        // Para cada nivel de escalón
         for (int y = 0; y < steps; y++)
         {
             // Generar los cubos desde el suelo hasta la altura del escalón actual
             for (int x = 0; x <= y; x++)
             {
-                
-                    // Colocar el cubo en la posición (x = y, y = height, z = 0)
-                    Vector3 position = new Vector3(y, x, z);
-                Instantiate(cubo, position, Quaternion.identity);
-                
-                
-                
+
+                // Colocar el cubo en la posición (x = y, y = height, z = 0)
+                Vector3 position = new Vector3(y, x, large);
+                Instantiate(cube, position, Quaternion.identity);
+
+
+
             }
         }
     }
-    
+
+    public void MapGenerate()
+    {
+
+
+    }
 }
